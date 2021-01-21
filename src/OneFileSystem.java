02c32b6b55614b49fbbb7c5ddf34f535cf6cd4bf
kaijun.yang <kaijun.yang@wattsine.com>
2021-01-21 10:30:20 +0800

    改为AIO前

---
+++ src/OneFileSystem.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneFileSystem {
+
+	private String SysFilePath = "." + File.separator + "updatabin" + File.separator;// ./ ：当前目录
+
+	public OneFileSystem() {
+
+	}
+
+	/**
+	 * 获取路径下的所有文件/文件夹
+	 * 
+	 * @param directoryPath  需要遍历的文件夹路径
+	 * @param isAddDirectory 是否将子文件夹的路径也添加到list集合中
+	 * @return
+	 */
+	public List<String> getAllbinFile(String directoryPath, boolean isAddDirectory) {
+		List<String> list = new ArrayList<String>();
+		File baseFile = new File(directoryPath);
+		if (baseFile.isFile() || !baseFile.exists()) {
+			return list;
+		}
+		File[] files = baseFile.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				if (isAddDirectory) {
+					list.add(file.getAbsolutePath());
+				}
+				list.addAll(getAllbinFile(file.getAbsolutePath(), isAddDirectory));
+			} else {
+				String filename = file.getAbsolutePath();
+				if (filename.endsWith(".bin")) {
+					list.add(filename);
+				}
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 获取路径下的所有文件/文件夹
+	 * 
+	 * @param directoryPath  需要遍历的文件夹路径
+	 * @param isAddDirectory 是否将子文件夹的路径也添加到list集合中
+	 * @return
+	 */
+	public List<String> getBinInBasePath() {
+
+		List<String> list = new ArrayList<String>();
+		File baseFile = new File(SysFilePath);
+//		try {
+//			System.out.println(baseFile.getCanonicalPath());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.println(System.getProperty("user.dir"));
+		if (baseFile.isFile() || !baseFile.exists()) {
+			return list;
+		}
+		File[] files = baseFile.listFiles();
+		for (File file : files) {
+			String filename = file.getAbsolutePath();
+			if (filename.endsWith(".bin")) {
+				list.add(file.getName());
+			}
+		}
+		return list;
+	}
+
+	public String SysbinFilePathgeter() {
+		return SysFilePath;
+	}
+
+}
---
+++ src/OneLogManager.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OneLogManager {
+	File Logfile;
+	FileOutputStream logWriteStream;
+
+	public OneLogManager() {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date(System.currentTimeMillis());
+		Logfile = new File("." + File.separator + "log" + File.separator + "log_" + formatter.format(date) + ".txt");
+		// 文件夹不存在则创建
+		if (!Logfile.getParentFile().exists()) {
+			Logfile.getParentFile().mkdirs();
+		}
+
+		if (!Logfile.exists()) {
+			try {
+				Logfile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			logWriteStream = new FileOutputStream(Logfile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void putLogcat(String logmsg) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date(System.currentTimeMillis());
+		logmsg = formatter.format(date) + ":" + logmsg + "\n";
+		byte[] msgbyte = logmsg.getBytes();
+		try {
+			logWriteStream.write(msgbyte, 0, msgbyte.length);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("LogManager,文件打开异常,撰写通道关闭，请重启服务器");
+			try {
+				logWriteStream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+}
